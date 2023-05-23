f71f0f44a5aad16fa7c0c024793a9737d02fb0db
Sergtran <sergiiosolano@hotmail.com>
2023-05-23 12:21:40 -0500

    tutorialSteps

---
+++ HumanSurvivor/Assets/HumanSurvivorScrips/ScriptablesTutorial/TutorialStepsScritable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "StepTutorial", menuName = "HumanSurvivor/StepTutorial")]
+
+public class TutorialStepsScritable : ScriptableObject
+{
+    public int idxStep;
+    public string nameStep;
+    public string descriptionStepForUI;
+    public bool isComplete;
+}
